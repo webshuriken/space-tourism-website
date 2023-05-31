59150b1113b22d1729029f94bf0ff519a4c23292
Carlos E Alford <carlos.alford@gmail.com>
2023-05-31 14:09:23 +0100

    setup file structure with assets

---
+++ src/components/Home/Home.js
@@ -0,0 +1,19 @@
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="Home">
+      So, you want to travel to
+  Space
+  <p>
+  Let’s face it; if you want to go to space, you might as well genuinely go to 
+  outer space and not hover kind of on the edge of it. Well sit back, and relax 
+  because we’ll give you a truly out of this world experience!
+  </p>
+
+  Explore
+    </div>
+  );
+}
+
+export default Home;
