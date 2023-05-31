02573e765da1d3d74d840c1d3a20efa3f15fe33a
Carlos E Alford <carlos.alford@gmail.com>
2023-05-31 14:08:39 +0100

    setup tailwindcss design system for the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./public/index.html",
+    "./src/index.js",
     "./src/components/**/*{js,jsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'midnight': '#0b0d17',
+        'tulip': '#d0d6f9',
+        'white': '#ffffff'
+      },
+      fontFamily: {
+        'bellefair': ['Bellefair'],
+        'barlow': ['Barlow Condensed']
+      }
+    }
   },
   plugins: [],
 }
