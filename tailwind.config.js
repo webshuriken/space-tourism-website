fb23a164142c499a7d9f309085a762c450878b31
Carlos E Alford <carlos.alford@gmail.com>
2023-06-06 10:37:52 +0100

    add setup for tailwind text

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,36 @@ module.exports = {
       },
       fontFamily: {
         'bellefair': ['Bellefair'],
-        'barlow': ['Barlow Condensed']
+        'barlow': ['Barlow Condensed'],
+        'h1': ['Bellefair'],
+        'h2': ['Bellefair'],
+        'h3': ['Bellefair'],
+        'h4': ['Bellefair'],
+        'h5': ['Barlow'],
+        'sub-head-1': ['Bellefair'],
+        'sub-head-2': ['Barlow'],
+        'nav': ['Barlow'],
+      },
+      fontSize: {
+        'h1-sm': '80px',
+        'h1-lg': '150px',
+        'h2-sm': '56px',
+        'h2-lg': '100px',
+        'h3-sm': '24px',
+        'h3-lg': '56px',
+        'h4-sm': '16px',
+        'h4-lg': '32px',
+        'h5-sm': '16px',
+        'h5-lg': '28px',
+        'sub-head-1': '28px',
+        'sub-head-2': '14px',
+        'nav': '16px',
+        'p': '18px'
+      },
+      letterSpacing: {
+        'h5': '4.72px',
+        'sub-head-2': '2.36px',
+        'nav': '2.7px'
       }
     }
   },
