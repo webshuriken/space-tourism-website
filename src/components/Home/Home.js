390fad767bde4ab5331419cb23ed89f283455ef5
Carlos E Alford <carlos.alford@gmail.com>
2023-06-07 12:19:59 +0100

    tablet layout for home page ready

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -12,18 +12,18 @@ function Home() {
 // TODO: WORKING ON THE APP PADDING
   return (
     <section id="home-wrapper" className='app-page flex flex-col items-center pt-[112px] md:pt-[203px] px-[24px] text-center' style={{backgroundImage: `url(${bgImage})`}}>
-      <div className='py-1 md:h-[250px]'>
+      <div className='py-1 md:h-[228px]'>
         <h5 className='font-h5 text-h5-sm md:text-h5-md lg:text-h5-lg tracking-h5-sm md:tracking-h5-md lg:tracking-h5-lg uppercase'>So, you want to travel to</h5>
-        <h1 className='font-h1 py-[12px] text-h1-sm md:text-h1-lg leading-[100px] md:leading-[150px] uppercase'>Space</h1>
+        <h1 className='font-h1 pt-[12px] md:pt-[16px] text-h1-sm md:text-h1-lg leading-[100px] md:leading-[150px] uppercase'>Space</h1>
       </div>
-      <div className='max-w-[450px] md:text-p-md lg:text-p-lg leading-p-sm'>
+      <div className='max-w-[450px] md:text-p-md lg:text-p-lg leading-p-sm md:leading-[28px]'>
         <p>
         Let’s face it; if you want to go to space, you might as well genuinely go to 
         outer space and not hover kind of on the edge of it. Well sit back, and relax 
         because we’ll give you a truly out of this world experience!
         </p>
       </div>
-      <button className='explore-btn mt-[80px] bg-white text-midnight uppercase'>Explore</button>
+      <button className='explore-btn mt-[80px] md:mt-[140px] bg-white text-midnight uppercase'>Explore</button>
     </section>
   );
 }
