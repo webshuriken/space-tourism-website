40e81673fa390435427d2e791e7f3267e3469060
Carlos E Alford <carlos.alford@gmail.com>
2023-06-07 15:27:07 +0100

    tailwindcss setup for home page ready

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,10 +19,10 @@ module.exports = {
         'h2': ['Bellefair'],
         'h3': ['Bellefair'],
         'h4': ['Bellefair'],
-        'h5': ['Barlow'],
+        'h5': ['Barlow Condensed'],
         'sub-head-1': ['Bellefair'],
-        'sub-head-2': ['Barlow'],
-        'nav': ['Barlow'],
+        'sub-head-2': ['Barlow Condensed'],
+        'nav': ['Barlow Condensed'],
       },
       fontSize: {
         'h1-sm': '5.333rem',
@@ -31,20 +31,26 @@ module.exports = {
         'h2-lg': '6.666rem',
         'h3-sm': '1.6rem',
         'h3-lg': '3.733rem',
-        'h4-sm': '0.1666rem',
+        'h4-sm': '1.1666rem',
         'h4-lg': '2.133rem',
-        'h5-sm': '0.1666rem',
-        'h5-lg': '1.866rem',
+        'h5-sm': '1.07rem',
+        'h5-md': '1.34rem',
+        'h5-lg': '1.87rem',
         'sub-head-1': '1.866rem',
-        'sub-head-2': '0.933rem',
-        'nav': '0.1666rem',
-        'p-md': '0.1666rem',
+        'sub-head-2': '1.933rem',
+        'nav': '1.07rem',
+        'p-md': '1.07rem',
         'p-lg': '1.2rem'
       },
       letterSpacing: {
-        'h5': '4.72px',
+        'h5-sm': '2.7px',
+        'h5-md': '3.38px',
+        'h5-lg': '4.72px',
         'sub-head-2': '2.36px',
         'nav': '2.7px'
+      },
+      lineHeight: {
+        'p-sm': '25px'
       }
     }
   },
