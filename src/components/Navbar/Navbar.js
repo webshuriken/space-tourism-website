69bb3a94084e055b8a5fa6b0617c835976875f1e
Carlos E Alford <carlos.alford@gmail.com>
2023-06-12 14:22:54 +0100

    navbar nav logic complete, mobile menu shows and hides on button press

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,5 @@
 import { Link, useLocation } from 'react-router-dom';
+import { useState } from 'react';
 import './Navbar.css';
 import logo from '../../assets/shared/logo.svg';
 import hamburgerBtn from '../../assets/shared/icon-hamburger.svg';
@@ -9,15 +10,21 @@ const AppPages = ['Home', 'Destination', 'Crew', 'Technology'];
 
 export default function Navbar() {
   const location = useLocation();
-  const currentPage = location.pathname;
+  const [mobileMenu, setMobileMenu] = useState('disabled');
+
+  function handleMobileMenu() {
+    if (window.innerWidth < 768) {
+      setMobileMenu(() => mobileMenu === 'active' ? 'disabled' : 'active');
+    }
+  }
 
   return (
     <nav className='navbar absolute flex top-0 left-0 items-center justify-between h-[88px] md:h-[96px] w-full px-[24px] md:px-[38px] lg:mt-[40px]'>
       <img src={logo} className='h-[40px] w-[40px] md:h-[48px] md:w-[48px]' alt="app logo that consists for a white circle" />
-      <button className='navbar-menu-btn h-[40px] w-[40px] py-[9.5px] px-[8px] md:hidden md:absolute'>
-        <img src={hamburgerBtn} alt="hamburger button" />
+      <button className='navbar-menu-btn h-[40px] w-[40px] py-[9.5px] px-[8px] md:hidden md:absolute' onClick={handleMobileMenu}>
+        <img src={mobileMenu === 'active' ? closeBtn : hamburgerBtn} alt="hamburger button" />
       </button>
-      <ul className='navbar-nav absolute top-0 right-0 flex flex-col md:flex-row md:items-center md:justify-between h-screen lg:justify-evenly md:h-[96px] w-[68%] md:w-[450px] lg:w-[830px] mr-[-1000px] md:mr-0 pt-[118px] pl-[28px] md:pt-0 md:px-[48px] font-barlow-condensed lg:text-nav uppercase'>
+      <ul className={`${mobileMenu} navbar-nav absolute top-0 right-0 flex flex-col md:flex-row md:items-center md:justify-between h-screen lg:justify-evenly md:h-[96px] w-[68%] md:w-[450px] lg:w-[830px] md:mr-0 pt-[118px] pl-[28px] md:pt-0 md:px-[48px] font-barlow-condensed lg:text-nav uppercase`}>
         {
           AppPages.map((page, i) => {
             const path = location.pathname;
