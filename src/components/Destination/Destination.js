04ced1b233c276a0848656a2632ebfdff72c5614
Carlos E Alford <carlos.alford@gmail.com>
2023-06-14 11:30:38 +0100

    destination page layout ready for mobile devices

---
+++ src/components/Destination/Destination.js
@@ -0,0 +1,52 @@
+import './Destination.css';
+import bgMobile from '../../assets/destination/background-destination-mobile.jpg';
+import bgTablet from  '../../assets/destination/background-destination-tablet.jpg';
+import bgDesktop from '../../assets/destination/background-destination-desktop.jpg';
+import useBackgroundImage from '../../hooks/useBackgroundImage';
+import appData from '../../data/data.json';
+import { useState } from 'react';
+import DestinationMenu from '../DestinationMenu/DestinationMenu';
+
+
+export default function Destination() {
+  // make the image responsive
+  const bgImage = useBackgroundImage({bgList: [bgMobile, bgTablet, bgDesktop]});
+  // on first load, we will default to the first destination in the list
+  const [destination, setDestination] = useState(appData.destinations[0]);
+  const webp = require(`${destination.images.png}`);
+  const png = require(`${destination.images.png}`);
+
+  function onClickChangeDestination(e) {
+    console.log("E: ", e.target.value)
+  }
+
+  return (
+    <section className="app-page pt-[88px] md:pt-[203px]" style={{backgroundImage: `url(${bgImage})`}}>
+      <h5 className='font-h5 text-h5-sm md:text-h5-md lg:text-h5-lg tracking-h5-sm md:tracking-h5-md lg:tracking-h5-lg uppercase text-center'>
+        <span className='font-bold pr-2 opacity-40'>01</span> Pick your destination
+      </h5>
+      <article className='grid grid-cols-[100%] grid-rows-[170px_auto] place-items-center gap-4 pt-8'>
+        <picture className='block realtive h-[170px] w-[170px]'>
+          <source srcSet={webp} type='image/webp' />
+          <img src={png} alt={`image of the ${destination.name}`} />
+        </picture>
+        <div className='grid  place-items-center grid-rows-[repeat(5, auto)] pt-[6px]'>
+          <DestinationMenu list={appData.destinations} handleClick={onClickChangeDestination} />
+          <h2 className='pt-[12px] font-h2 text-h2-sm md:text-h2-md lg:text-h2-lg uppercase'>{destination.name}</h2>
+          <div>
+            <p className='md:text-p-md lg:text-p-lg leading-p-sm md:leading-p-md lg:leading-p-lg text-center'>{destination.description}</p>
+          </div>
+          <span className='h-[1px] w-full my-[32px] bg-white opacity-40'></span>
+          <div className='w-full text-center'>
+            <p className='font-sub-head-2 text-sub-head-2 tracking-sub-head-2 uppercase'>AVG. DISTANCE</p>
+            <p className='font-sub-head-1 text-sub-head-1 uppercase'>{destination.distance}</p>
+          </div>
+          <div className='w-full py-[32px] text-center'>
+            <p className='font-sub-head-2 text-sub-head-2 tracking-sub-head-2 uppercase'>EST. TRAVEL TIME</p>
+            <p className='font-sub-head-1 text-sub-head-1 uppercase'>{destination.travel}</p>
+          </div>
+        </div>
+      </article>
+    </section>
+  )
+}
