170951a5978f006c191eb38f4a7238d2fb9a2915
Carlos E Alford <carlos.alford@gmail.com>
2023-06-14 12:06:40 +0100

    destination page layout ready for tablet sizes

--- src/components/Destination/Destination.js
+++ src/components/Destination/Destination.js
@@ -21,29 +21,31 @@ export default function Destination() {
   }
 
   return (
-    <section className="app-page pt-[88px] md:pt-[203px]" style={{backgroundImage: `url(${bgImage})`}}>
-      <h5 className='font-h5 text-h5-sm md:text-h5-md lg:text-h5-lg tracking-h5-sm md:tracking-h5-md lg:tracking-h5-lg uppercase text-center'>
+    <section className="app-page pt-[88px] md:pt-[130px]" style={{backgroundImage: `url(${bgImage})`}}>
+      <h5 className='font-h5 text-h5-sm md:text-h5-md lg:text-h5-lg tracking-h5-sm md:tracking-h5-md lg:tracking-h5-lg uppercase text-center md:text-left'>
         <span className='font-bold pr-2 opacity-40'>01</span> Pick your destination
       </h5>
-      <article className='grid grid-cols-[100%] grid-rows-[170px_auto] place-items-center gap-4 pt-8'>
-        <picture className='block realtive h-[170px] w-[170px]'>
+      <article className='grid grid-cols-[100%] grid-rows-[170px_auto] md:grid-cols-[574px] md:grid-rows-[300px_auto] place-items-center place-content-center gap-4 md:gap-12 pt-8 md:pt-16'>
+        <picture className='block realtive h-[170px] w-[170px] md:h-[300px] md:w-[300px]'>
           <source srcSet={webp} type='image/webp' />
           <img src={png} alt={`image of the ${destination.name}`} />
         </picture>
         <div className='grid  place-items-center grid-rows-[repeat(5, auto)] pt-[6px]'>
           <DestinationMenu list={appData.destinations} handleClick={onClickChangeDestination} />
-          <h2 className='pt-[12px] font-h2 text-h2-sm md:text-h2-md lg:text-h2-lg uppercase'>{destination.name}</h2>
+          <h2 className='pt-[12px] md:pt-8 font-h2 text-h2-sm md:text-h2-md lg:text-h2-lg uppercase'>{destination.name}</h2>
           <div>
             <p className='md:text-p-md lg:text-p-lg leading-p-sm md:leading-p-md lg:leading-p-lg text-center'>{destination.description}</p>
           </div>
           <span className='h-[1px] w-full my-[32px] bg-white opacity-40'></span>
-          <div className='w-full text-center'>
-            <p className='font-sub-head-2 text-sub-head-2 tracking-sub-head-2 uppercase'>AVG. DISTANCE</p>
-            <p className='font-sub-head-1 text-sub-head-1 uppercase'>{destination.distance}</p>
-          </div>
-          <div className='w-full py-[32px] text-center'>
-            <p className='font-sub-head-2 text-sub-head-2 tracking-sub-head-2 uppercase'>EST. TRAVEL TIME</p>
-            <p className='font-sub-head-1 text-sub-head-1 uppercase'>{destination.travel}</p>
+          <div className='flex flex-col md:flex-row gap-[32px] md:gap-0 pb-16 md:pb-8 w-full'>
+            <div className='w-full text-center'>
+              <p className='font-sub-head-2 text-sub-head-2 tracking-sub-head-2 uppercase'>AVG. DISTANCE</p>
+              <p className='font-sub-head-1 text-sub-head-1 uppercase'>{destination.distance}</p>
+            </div>
+            <div className='w-full text-center'>
+              <p className='font-sub-head-2 text-sub-head-2 tracking-sub-head-2 uppercase'>EST. TRAVEL TIME</p>
+              <p className='font-sub-head-1 text-sub-head-1 uppercase'>{destination.travel}</p>
+            </div>
           </div>
         </div>
       </article>
