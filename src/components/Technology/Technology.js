c273ae412c714c33393b5c104637a0956ad8e5fe
Carlos E Alford <carlos.alford@gmail.com>
2023-06-22 19:26:26 +0100

    style: Tehcnology component is now fully responsive

--- src/components/Technology/Technology.js
+++ src/components/Technology/Technology.js
@@ -32,7 +32,7 @@ export default function Technology() {
         setTechnology(state => newTechnology);
       }
     }catch(e) {
-      throw new Error('Failed to map through the app destination succesfully');
+      throw new Error('Failed to map through the app technology succesfully');
     }
   }
 
@@ -55,20 +55,24 @@ export default function Technology() {
   }, [])
 
   return (
-    <section className="app-page flex flex-row justify-center pt-[88px] md:pt-[130px] lg:pt-[208px] lg:px-8" style={{backgroundImage: `url(${bgImage})`}}>
-      <div className='w-full max-w-[1138px]'>
-        <h5 className='font-h5 text-h5-sm md:text-h5-md lg:text-h5-lg tracking-h5-sm md:tracking-h5-md lg:tracking-h5-lg uppercase text-center md:text-left'>
+    <section className="app-page technology-page flex flex-row justify-center pt-[88px] md:pt-[130px] lg:pt-[208px] lg:px-8" style={{backgroundImage: `url(${bgImage})`}}>
+      <div className='w-full max-w-[1440px]'>
+        <h5 className='max-w-[1138px] px-[24px] font-h5 text-h5-sm md:text-h5-md lg:text-h5-lg tracking-h5-sm md:tracking-h5-md lg:tracking-h5-lg uppercase text-center md:text-left'>
           <span className='font-bold pr-2 opacity-40'>03</span> Space launch 101
         </h5>
-        <article className='grid lg:grid-cols-[2fr,1fr] lg:grid-rows-[248px_repeat(2,200px)] pt-[32px]'>
-          <img src={(screenOrientation === 'portrait' ? portrait : landscape)} className='block h-auto w-auto lg:w-full lg:h-max' alt={`image of a ${technology.name}`} />
-          <TechnologyMenu technologyList={technologyList} handleClick={loadTechnology} />
-          <div className='md:order-1 pt-[32px] md:pt-0 uppercase text-center lg:self-end lg:text-left lg:col-start-1 lg:col-span-1 lg:row-start-1 lg:row-span-1'>
-            <h4 className='font-h4 text-h4-sm md:text-h4-md lg:text-h4-lg opacity-60'>The terminology ...</h4>
-            <h3 className='font-h3 text-h3-sm md:text-h3-md lg:text-h3-lg'>{technology.name}</h3>
+        <article className='grid pt-[32px] lg:grid-cols-[2fr,1fr] lg:grid-rows-[58px_auto_58px] xl:grid-cols-[auto_515px] xl:grid-rows-[114px_auto_114px]'>
+          <div className='relative grid content-end h-[170px] overflow-hidden md:h-[310px] lg:col-start-2 lg:col-span-1 lg:row-start-1 lg:row-span-3 lg:content-center lg:order-2 xl:h-full'>
+            <img src={(screenOrientation === 'portrait' ? portrait : landscape)} className='block h-auto w-full xl:h-full xl:w-full' alt={`image of a ${technology.name}`} />
           </div>
-          <div className='justify-self-center md:order-2 md:w-[458px] pt-[22px] pb-12 md:pt-[10px] md:pb-0 md:text-p-md lg:text-p-lg leading-p-sm md:leading-p-md lg:leading-p-lg text-center md:text-left lg:justify-self-start lg:col-start-1 lg:col-span-1 lg:row-start-2 lg:row-span-1 lg:w-[454px]'>
-            <p>{technology.description}</p>
+          <div className='grid px-[24px] lg:grid-cols-[124px,auto] lg:grid-rows-[auto,auto] lg:col-start-1 lg:col-span-1 lg:row-start-2 lg:row-span-1 lg:order-1 xl:grid-cols-[188px,auto]'>
+            <TechnologyMenu technologyList={technologyList} handleClick={loadTechnology} />
+            <div className='pt-5 text-center lg:col-start-2 lg:col-span-1 lg:row-start-1 lg:row-span-1 lg:text-left uppercase'>
+              <h4 className='font-barlow-condensed text-[.94rem] tracking-[2.36px] md:text-[1.06rem] md:tracking-[2.7px]'>The terminology ...</h4>
+              <h3 className='font-h3 text-h3-sm md:text-h3-md lg:text-h3-lg'>{technology.name}</h3>
+            </div>
+            <div className='pt-5 leading-p-sm text-center md:justify-self-center md:w-[458px] md:text-p-md md:leading-p-md md:text-left lg:justify-self-start lg:col-start-2 lg:col-span-1 lg:row-start-2 lg:row-span-1 lg:text-p-lg lg:leading-p-lg'>
+              <p>{technology.description}</p>
+            </div>
           </div>
         </article>
       </div>
