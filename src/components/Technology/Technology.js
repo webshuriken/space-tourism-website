994051de83dcf4d46b43700472c6907ed3ce3d5f
Carlos E Alford <carlos.alford@gmail.com>
2023-07-03 18:11:17 +0100

    feat: implements the loading spinner on all pages
    to give the feel of something happening
    even if the data is already loaded the spinner will show for 400ms

--- src/components/Technology/Technology.js
+++ src/components/Technology/Technology.js
@@ -6,6 +6,8 @@ import useBackgroundImage from '../../hooks/useBackgroundImage';
 import appData from '../../data/data.json';
 import { useEffect, useState } from 'react';
 import TechnologyMenu from '../TechnologyMenu/TechnologyMenu';
+import { Circles } from 'react-loader-spinner';
+import { useSpinner } from '../../hooks/userSpinner';
 
 
 export default function Technology() {
@@ -13,6 +15,8 @@ export default function Technology() {
   const [technologyList, setTechnologyList] = useState(appData.technology);
   const [technology, setTechnology] = useState(appData.technology[0]);
   const [screenOrientation, setScreenOrientation] = useState('landscape');
+  // use the spinner custom hook
+  const spinnerLoading = useSpinner();
   // resposive page background image
   const bgImage = useBackgroundImage({bgList: [bgMobile, bgTablet, bgDesktop]});
   const portrait = require(`${technology.images.portrait}`);
@@ -62,21 +66,34 @@ export default function Technology() {
         <h5 className='max-w-[1138px] px-[24px] font-h5 text-h5-sm md:text-h5-md lg:text-h5-lg tracking-h5-sm md:tracking-h5-md lg:tracking-h5-lg uppercase text-center md:text-left'>
           <span className='font-bold pr-2 opacity-40'>03</span> Space launch 101
         </h5>
-        <article className='grid pt-[32px] lg:grid-cols-[1fr,1fr] lg:grid-rows-[58px_auto_58px] xl:grid-cols-[auto_515px] xl:grid-rows-[114px_auto_114px]'>
-          <div className='relative grid content-end h-[170px] overflow-hidden md:h-[310px] lg:col-start-2 lg:col-span-1 lg:row-start-1 lg:row-span-3 lg:content-center lg:self-center lg:order-2 xl:h-full'>
-            <img src={(screenOrientation === 'portrait' ? portrait : landscape)} className='block h-auto w-full xl:h-full xl:w-full' alt={`image of a ${technology.name}`} />
-          </div>
-          <div className='grid px-[24px] lg:grid-cols-[124px,auto] lg:grid-rows-[auto,auto] lg:col-start-1 lg:col-span-1 lg:row-start-2 lg:row-span-1 lg:order-1 xl:grid-cols-[188px,auto]'>
-            <TechnologyMenu technologyList={technologyList} handleClick={loadTechnology} />
-            <div className='pt-5 text-center lg:col-start-2 lg:col-span-1 lg:row-start-1 lg:row-span-1 lg:text-left uppercase'>
-              <h4 className='font-barlow-condensed text-[.94rem] tracking-[2.36px] md:text-[1.06rem] md:tracking-[2.7px]'>The terminology ...</h4>
-              <h3 className='font-h3 text-h3-sm md:text-h3-md xl:text-h3-lg'>{technology.name}</h3>
-            </div>
-            <div className='pt-5 leading-p-sm text-center md:justify-self-center md:w-[458px] md:text-p-md md:leading-p-md md:text-left lg:justify-self-start lg:col-start-2 lg:col-span-1 lg:row-start-2 lg:row-span-1 lg:text-p-lg lg:leading-p-lg'>
-              <p>{technology.description}</p>
-            </div>
-          </div>
-        </article>
+        {
+          spinnerLoading
+            ?
+              <Circles
+                height="80"
+                width="80"
+                color="#ffffff"
+                ariaLabel="circles-loading"
+                wrapperClass="justify-center items-center h-3/5"
+                visible={true}
+              />
+            :
+              <article className='grid pt-[32px] lg:grid-cols-[1fr,1fr] lg:grid-rows-[58px_auto_58px] xl:grid-cols-[auto_515px] xl:grid-rows-[114px_auto_114px]'>
+                <div className='relative grid content-end h-[170px] overflow-hidden md:h-[310px] lg:col-start-2 lg:col-span-1 lg:row-start-1 lg:row-span-3 lg:content-center lg:self-center lg:order-2 xl:h-full'>
+                  <img src={(screenOrientation === 'portrait' ? portrait : landscape)} className='block h-auto w-full xl:h-full xl:w-full' alt={`image of a ${technology.name}`} />
+                </div>
+                <div className='grid px-[24px] lg:grid-cols-[124px,auto] lg:grid-rows-[auto,auto] lg:col-start-1 lg:col-span-1 lg:row-start-2 lg:row-span-1 lg:order-1 xl:grid-cols-[188px,auto]'>
+                  <TechnologyMenu technologyList={technologyList} handleClick={loadTechnology} />
+                  <div className='pt-5 text-center lg:col-start-2 lg:col-span-1 lg:row-start-1 lg:row-span-1 lg:text-left uppercase'>
+                    <h4 className='font-barlow-condensed text-[.94rem] tracking-[2.36px] md:text-[1.06rem] md:tracking-[2.7px]'>The terminology ...</h4>
+                    <h3 className='font-h3 text-h3-sm md:text-h3-md xl:text-h3-lg'>{technology.name}</h3>
+                  </div>
+                  <div className='pt-5 leading-p-sm text-center md:justify-self-center md:w-[458px] md:text-p-md md:leading-p-md md:text-left lg:justify-self-start lg:col-start-2 lg:col-span-1 lg:row-start-2 lg:row-span-1 lg:text-p-lg lg:leading-p-lg'>
+                    <p>{technology.description}</p>
+                  </div>
+                </div>
+              </article>
+        }
       </div>
     </section>
   )
